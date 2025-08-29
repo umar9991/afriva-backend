68727fb656d59a593a7c9fc68496ed21ec0c2108
umarahmad9991 <umarahmad9u8@gmail.com>
2025-08-29 23:20:43 +0400

    adding vercel json

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+const router = express.Router();
+const { sendVerificationCode, verifyOtp } = require('../controllers/authController');
+
+router.post('/signup', authController.signup);
+router.post('/signin', authController.signin);
+router.post('/signout', authController.signout);
+
+router.patch('/sendVerificationCode', authController.sendVerificationCode);
+router.post('/verify-otp', verifyOtp);
+
+module.exports = router;
